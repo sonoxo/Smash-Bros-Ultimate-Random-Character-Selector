9abb1fb77eb9b83a516ca97472fdead2ca3408cc
Douglas Brown .Jr <60854716+sonoxo@users.noreply.github.com>
2026-09-22 12:55:20 -0400

    Add Sonoxo platform fighter prototype

---
+++ Source/SmashRandomiser/SmashRandomiser.Build.cs
@@ -0,0 +1,10 @@
+using UnrealBuildTool;
+
+public class SmashRandomiser : ModuleRules
+{
+    public SmashRandomiser(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    }
+}
