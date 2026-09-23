e803fbf90496175d30fefe8eec61f42e0b80b898
Douglas Brown .Jr <60854716+sonoxo@users.noreply.github.com>
2026-09-23 10:35:27 -0400

    Upgrade editor target for Unreal Engine 5.8

--- Source/SmashRandomiserEditor.Target.cs
+++ Source/SmashRandomiserEditor.Target.cs
@@ -6,7 +6,8 @@ public class SmashRandomiserEditorTarget : TargetRules
     public SmashRandomiserEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
         ExtraModuleNames.Add("SmashRandomiser");
     }
 }
